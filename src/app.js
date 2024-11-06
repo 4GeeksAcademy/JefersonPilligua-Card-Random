17c1d6553f7c32a7d41a4e8562c71998961d1aef
Jeferson Pilligua <jefer.pilligua1509@gmail.com>
2024-11-06 13:50:00 +0000

    progreso

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateRandomCard() {
+    const suits = ["♥️", "♠️", "♣️", "♦️"];
+    const num = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomNum = num[Math.floor(Math.random() * num.length)];
+
+    return `${randomSuit} ${randomNum}`;
+  }
+
+  function viewRandomCard() {
+    const elementCard = document.getElementById("card");
+    elementCard.textContent = generateRandomCard();
+  }
+
+  window.addEventListener("load", viewRandomCard);
 };
